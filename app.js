fe952e4df99646e3471e60e1a7f434e5c61d89a5
Onakoya Kayode <onakoyakayode@gmail.com>
2022-06-22 19:21:59 +0100

    toggle button responsiveness

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+
+const toggleBtn = document.querySelector('.toggle-btn')
+const navBar = document.querySelector('ul.navlist')
+const getStarted = document.querySelector('div.get-started')
+
+
+toggleBtn.addEventListener('click', (e) => {
+  if (navBar.style.left === "-120%") {
+    navBar.style.left = "0%"
+  } else {
+    navBar.style.left = "-120%"
+  }
+
+
+  if (getStarted.style.left === "-100%") {
+    getStarted.style.left = "31%"
+  } else {
+    getStarted.style.left = "-100%"
+  }
+})
+
+
+
+
+
